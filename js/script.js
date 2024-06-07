15a254054759769eb07352447b709f0983b47e38
ctaylor <hi.ctaylor.co@gmail.com>
2024-06-07 15:24:06 -0500

    Added JS for assigning dishes using loop and event handler

--- js/script.js
+++ js/script.js
@@ -11,37 +11,73 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+const assignButton = document.querySelector(".assign");
+
+const assignedItems = document.querySelector(".assigned-items");
+
 addGuestButton.addEventListener("click", function () {
-    const guest = guestInput.value;
-    //console.log(guest);
+  const guest = guestInput.value;
+  //console.log(guest);
 
-    if(guest !== "") {
-        addToList(guest);
-    }
+  if (guest !== "") {
+    addToList(guest);
+  }
 
-    clearInput ();
+  clearInput();
 
-    updateGuestCount();
+  updateGuestCount();
 });
 
 const clearInput = function () {
-    guestInput.value = "";
-}
+  guestInput.value = "";
+};
 
 const addToList = function (guest) {
-    const listItem = document.createElement("li");
-        listItem.innerText = guest;
-        guestList.append(listItem);
-}
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
 
 const updateGuestCount = function () {
-    const guests = document.querySelectorAll(".guest-list li");
-    guestCount.innerText = guests.length;
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
 
-    if(guests.length === 8) {
-        addGuestButton.classList.add("hide");
-        guestInput.classList.add("hide");
-        guestInputLabel.classList.add("hide");
-        guestFull.classList.remove("hide");
-    }
-}
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "casserole",
+    "fried chicken",
+    "devilled eggs",
+    "salad",
+    "steamed veggies",
+    "fruit",
+    "veggie tray",
+    "charcuterie",
+    "rolls",
+    "cookies",
+    "brownies",
+    "chocolate delight",
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems.splice(randomPotluckIndex, 1);
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
